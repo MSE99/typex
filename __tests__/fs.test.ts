c0d827d36d7524a5f19a71f3feb62a2fe226b715
mse99 <mohamedsalemedrah@gmail.com>
2024-11-12 09:40:51 +0200

    Install & setup vitest and write test for fs module

---
+++ __tests__/fs.test.ts
@@ -0,0 +1,35 @@
+import { describe, test, beforeAll, expect } from "vitest";
+import Path from "path";
+import OS from "os";
+import Crypto from "crypto";
+import Fs from "fs";
+import { readFilenames } from "../src/fs";
+
+describe("fs", () => {
+    const prepTestDir = () => {
+        const testDirPath = Path.join(OS.tmpdir(), `typex-test-dir-${Crypto.randomInt(5_000_000)}`);
+        Fs.mkdirSync(testDirPath);
+        return testDirPath;
+    };
+
+    test("readFilenames: [] for empty dir.", async () => {
+        const files = await readFilenames(prepTestDir(), "*");
+        expect(files).toEqual([]);
+    });
+
+    test("readFilenames: should match ts files.", async () => {
+        const path = prepTestDir();
+
+        Fs.writeFileSync(Path.join(path, "1.ts"), "");
+        Fs.writeFileSync(Path.join(path, "2.ts"), "");
+        Fs.writeFileSync(Path.join(path, "3.ts"), "");
+        Fs.writeFileSync(Path.join(path, "4.js"), "");
+        Fs.writeFileSync(Path.join(path, "5.css"), "");
+        Fs.writeFileSync(Path.join(path, "6.html"), "");
+
+        const files = await readFilenames(path, "*.ts");
+        files.sort();
+
+        expect(files).toEqual(["1.ts", "2.ts", "3.ts"]);
+    });
+});
